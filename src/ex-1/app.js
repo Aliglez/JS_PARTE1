5ccee5d2978ee0d094e123ae3876baf01b0cdee0
Alicia González Castro <aliglezcastro@gmail.com>
2023-10-23 12:34:06 +0200

    funcition to print created

--- src/ex-1/app.js
+++ src/ex-1/app.js
@@ -1,3 +1,16 @@
 // Comprobamos que tenemos los archivos conectados
 
 console.log("conectado ejercicio 1")
+
+// Escribe un programa que escriba en la pantalla un texto de tipo <h1> que diga “Hello Javascript”.
+
+const title = "Hello JavaScript"
+const titleSection = document.getElementById('title')
+
+function printTitle() {
+    titleSection.innerHTML = /* html */`
+        <h1>${title}</h1>
+        `
+}
+
+printTitle()

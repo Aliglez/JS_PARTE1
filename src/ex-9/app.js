6a1a5143eb4f9f477419377693a2040c90bd69c0
Alicia González Castro <aliglezcastro@gmail.com>
2023-12-04 21:21:31 +0100

    ex8 completed

--- src/ex-9/app.js
+++ src/ex-9/app.js
@@ -2,7 +2,7 @@ document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
    
     let sentence = document.getElementById("text").value;
-    let count = (sentence.match(/[aeiou]/g) || []);
+    let count = (sentence.match(/[aeiou]/g) || []).length;
   
     let outputElement = document.getElementById("result");
     outputElement.innerHTML = "La frase tiene " + count + " vocales";
